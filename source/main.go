c5885c46dd9e7ab146e206d2a22a2dae259da5a8
Sagar Poojary <sagarpoojary1@gmail.com>
2022-09-17 21:05:28 +0530

    removed excel parse

--- source/main.go
+++ source/main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"fmt"
 
-	excelparse "github.com/Sagar848/excel-reader/excel"
+	"github.com/Sagar848/excel-reader/excel"
 )
 
 func main() {
 	fmt.Println("here we will read excel files")
 
-	header, rows := excelparse.Parse("D:/files/DummyFile.xlsx", false)
+	header, rows := excel.Parse("D:/files/DummyFile.xlsx", false)
 	fmt.Println("header =", header)
 	fmt.Println("rows =", rows)
 
