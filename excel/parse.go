f78229f2477294b9d032bbacceac12ee1cfde088
Sagar Poojary <sagarpoojary1@gmail.com>
2022-09-18 16:49:25 +0530

    changed rows and columns slice to map

--- excel/parse.go
+++ excel/parse.go
@@ -2,21 +2,23 @@ package excelparse
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/xuri/excelize/v2"
 )
 
 // Send the file you want to parse
 // File has to be provided with full path
-func Parse(filename string, hasHeader bool) ([]string, [][]string) {
+func Parse(filename string, hasHeader bool) []map[string]interface{} {
 
 	var header = make([]string, 0, 10)
-	var data = make([][]string, 0, 10)
+	// var data = make([][]string, 0, 10)
+	var dataMap []map[string]interface{} = []map[string]interface{}{}
 
 	f, err := excelize.OpenFile(filename)
 	if err != nil {
-		fmt.Println(err)
-		return header, data
+		log.Println("Error in opening file: ", err)
+		return nil
 	}
 	defer func() {
 		if err := f.Close(); err != nil {
@@ -31,8 +33,8 @@ func Parse(filename string, hasHeader bool) ([]string, [][]string) {
 	// Get all the rows in the Sheet1.
 	rows, err := f.GetRows(activeSheet)
 	if err != nil {
-		fmt.Println(err)
-		return header, data
+		log.Println("Error in getting rows: ", err)
+		return nil
 	}
 	for rowNum, row := range rows {
 
@@ -43,12 +45,23 @@ func Parse(filename string, hasHeader bool) ([]string, [][]string) {
 				for cellNum := range row {
 					header = append(header, fmt.Sprint("column", cellNum))
 				}
-				data = append(data, row)
+
+				dataMap = append(dataMap, createDataMap(header, row))
 			}
 		} else {
-			data = append(data, row)
+			dataMap = append(dataMap, createDataMap(header, row))
 		}
 
 	}
-	return header, data
+	return dataMap
+}
+
+func createDataMap(header []string, row []string) map[string]interface{} {
+	currentData := map[string]interface{}{}
+	if len(header) == len(row) {
+		for i, cell := range row {
+			currentData[header[i]] = cell
+		}
+	}
+	return currentData
 }
--- main.go
+++ main.go
@@ -9,8 +9,7 @@ import (
 func main() {
 	fmt.Println("here we will read excel files")
 
-	header, rows := excel.Parse("D:/files/DummyFile.xlsx", false)
-	fmt.Println("header =", header)
-	fmt.Println("rows =", rows)
+	dataMap := excel.Parse("D:/files/DummyFile.xlsx", false)
+	fmt.Println("dataMap =", dataMap)
 
 }
